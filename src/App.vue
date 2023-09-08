051683a6743c4230bae8c404d7cad2d39c00cabf
CodeDredd <gregor@codedredd.de>
2023-09-08 09:05:31 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    {{ users }}
+  </div>
+
+  <div>
+    <button @click="addUser">Add user</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import User from './stores/models/user';
+import { useRepo } from 'pinia-orm';
+import { computed } from 'vue';
+
+const userRepo = useRepo(User);
+
+// Getting all users
+const users = computed(() => userRepo.all());
+
+const addUser = () => {
+  userRepo.insert({
+    name: 'John Doe',
+    email: 'john@doe.com',
+  });
+};
+</script>
